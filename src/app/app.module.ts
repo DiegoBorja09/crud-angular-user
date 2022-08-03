c83bef9852539f8382e7ee0ac49b604099fd516f
DiegoBorja09 <diegoborja09@gmail.com>
2022-08-03 15:31:45 -0500

    Crud-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './component/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/user/user.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+accion='Agregar';
+id:number|undefined;
+  datos: any[] =[
+    {
+      id: '1',
+      nombre:'diego',
+      apellidos: 'borja',
+      identificacion:'1192716568'
+   },  {
+   id: '2',
+   nombre:'diego',
+   apellidos: 'borja',
+   identificacion:'1192716568'
+  },];
+
+  form: FormGroup;
+
+  constructor(private fb:FormBuilder) { this.form=this.fb.group({
+    id:['',Validators.required],
+    nombre:['',Validators.required],
+    apellidos:['',Validators.required],
+    identificacion:['',[Validators.required,Validators.maxLength(10),Validators.minLength(8)]]
+  })}
+
+  ngOnInit(): void {
+  }
+
+ Editar(user:any){
+    this.accion='Editar';
+    this.id=user.id;
+    this.form.patchValue({
+      id: user.id,
+      nombre:user.nombre,
+      apellidos: user.apellidos,
+      identificacion:user.identificacion
+    })
+    
+  }
+ 
+  
+  //metodo del boton eliminar.
+  Delete(index:number){
+    this.datos.splice(index,1)
+    Swal.fire({
+      icon: 'error',
+      title:'Eliminado',
+      text:'Registro Eliminado Correctamente',
+      timer:2000
+      
+    })
+    }
+
+    Guardar(){
+    const dato:any={
+      id: this.form.get('id')?.value,
+      nombre:this.form.get('nombre')?.value,
+      apellidos: this.form.get('apellidos')?.value,
+      identificacion:this.form.get('identificacion')?.value
+    }
+    if (this.id==undefined) {
+      this.datos.push(dato)
+      Swal.fire({
+        icon: 'success',
+        title:'Guardar',
+        text:'Registro Almacenado Correctamente',
+        timer:2000,
+        showConfirmButton: false,
+        
+      })
+      this.form.reset
+      
+    }else {
+      for (let user in this.datos) {
+        if (this.datos[user].id==this.form.get("id")?.value) {
+          this.datos[user].nombre=this.form.get('nombre')?.value,
+          this.datos[user].apellidos=this.form.get('apellidos')?.value,
+          this.datos[user].identificacion=this.form.get('identificacion')?.value
+          Swal.fire({
+            icon: 'info',
+            title:'Editado',
+            text:'Registro Editado Correctamente',
+            showConfirmButton: false,
+            timer:2000
+            
+          })
+          this.form.reset
+          
+        }
+        
+      }
+    } 
+    
+    }
+}
